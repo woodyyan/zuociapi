ba6b5abe4bae70c7cef141411ffa57f49d29025a
JessieDing <nathalie.ding@gmail.com>
2018-01-23 20:48:35 +0800

    second commit

---
+++ zuoci-api-boot/src/main/java/com/easystudio/api/zuoci/controller/PhraseController.java
@@ -0,0 +1,9 @@
+package com.easystudio.api.zuoci.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/brands/{brand}")
+public class PhraseController {
+}
