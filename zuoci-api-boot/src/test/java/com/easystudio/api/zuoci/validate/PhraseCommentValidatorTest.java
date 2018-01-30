a9de18f65b8cf8fed6d483216ceceb82009502a5
WoodyYan <colorguitar@hotmail.com>
2018-01-30 11:01:13 +0800

    Add spring logging.

---
+++ zuoci-api-boot/src/test/java/com/easystudio/api/zuoci/validate/PhraseCommentValidatorTest.java
@@ -0,0 +1,12 @@
+package com.easystudio.api.zuoci.validate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PhraseCommentValidatorTest {
+
+    @Test
+    public void validate() {
+    }
+}
